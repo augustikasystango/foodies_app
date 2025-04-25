a907ec7e0feff547b24608bdc255ac6f395bc12d
Augustika Pal <augustika.pal@systango.com>
2025-04-25 15:09:44 +0530

    added dynamic route

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,31 @@
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
 
-function IndexMealsPage() {
+
+function MealDetailsPage({params}) {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g,'<br/>');
+
   return(
-    <h2>Dynamic route</h2>
+   <>
+     <header className={classes.header}>
+      <div className={classes.image}>
+        <Image src={meal.image} alt={meal.title} fill />
+      </div>
+      <div className={classes.headerText}>
+        <h1>{meal.title}</h1>
+        <p className={classes.creator}>
+          by <a href={`mailto:${meal.creator_email}`}>Name</a>
+        </p>
+        <p className={classes.summary}>{meal.summary}</p>
+      </div>
+     </header>
+     <main>
+      <p className={classes.instructions} dangerouslySetInnerHTML={{__html:meal.instructions}}></p>
+     </main>
+   </>
   )
 }
-export default IndexMealsPage;
+export default MealDetailsPage;
 
