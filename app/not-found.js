e76edc9127ff0a9f88a80806e59f7166d9e9f1ea
Augustika Pal <augustika.pal@systango.com>
2025-04-25 12:40:10 +0530

    added not-found page

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      
+    </div>
+  )
+}
