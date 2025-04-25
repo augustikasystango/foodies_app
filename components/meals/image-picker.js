16c6fc2004d0809b373134085878d8830c95b6d3
Augustika Pal <augustika.pal@systango.com>
2025-04-25 17:20:48 +0530

    added image picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,9 +1,27 @@
 'use client'
 import classes from './image-picker.module.css'
-import { useRef } from 'react';
+import { useRef ,useState} from 'react';
+import Image from 'next/image';
+
 
 export default function ImagePicker({ label, name }) {
     const imageInputRef = useRef();
+    const [pickedImage,setPickedImage]=useState();
+
+    const handleImageChange=(event)=>{
+        event.preventDefault();
+        const file = event.target.files[0];
+        if(!file){
+            setPickedImage(null);
+            return 
+        }
+        const fileReader = new FileReader();
+
+        fileReader.onload=()=>{
+           setPickedImage( fileReader.result);
+        }
+        fileReader.readAsDataURL(file);
+    }
 
     const handlePickClick=()=> {
         imageInputRef.current.click(); 
@@ -11,9 +29,15 @@ export default function ImagePicker({ label, name }) {
     return (<div className={classes.picker}>
         <label htmlFor='image'>{label}</label>
         <div className={classes.controls}>
+            <div className={classes.preview}>
+                {!pickedImage && <p>No image picked!</p>}
+                {pickedImage && <Image src={pickedImage} alt="Image selected  by the user" fill/>}
+            </div>
             <input type='file' id='image' 
             className={classes.input}
-            accept='image/png , image/jpeg' name={name} ref={imageInputRef} />
+            required
+            accept='image/png , image/jpeg' name={name} ref={imageInputRef} 
+            onChange={handleImageChange}/>
             <button className={classes.button} type='button'onClick={handlePickClick}>Pick an Image</button>
         </div>
     </div>)
