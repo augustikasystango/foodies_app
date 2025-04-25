c14b7d007a3357f324e1f888d81860da4613f6b6
Augustika Pal <augustika.pal@systango.com>
2025-04-25 12:34:02 +0530

    added loading & error pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,24 +2,33 @@ import { getMeals } from '@/lib/meals';
 import classes from './page.module.css';
 import Link from 'next/link';
 import MealsGrid from '@/components/meals/meals-grid';
+import { Suspense } from 'react';
 
-const MealsPage = async()=>{
-   const meals = await getMeals();
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+}
+
+const MealsPage = () => {
 
 
-  
-  return(
+
+
+  return (
     <>
-        <header className={classes.header}>
-          <h1>Delicious mealse ,created {' '}<span className={classes.highlight}>by you</span></h1>
-          <p>Choose your favorite recipe and cook .It is easy and fun !</p>
-          <p className={classes.cta}>
-            <Link href="/meals/share">Share your favorite recipe</Link>
-          </p>
-        </header>
-        <main className={classes.main}>
-          <MealsGrid meals={meals}/>
-        </main>
+      <header className={classes.header}>
+        <h1>Delicious mealse ,created {' '}<span className={classes.highlight}>by you</span></h1>
+        <p>Choose your favorite recipe and cook .It is easy and fun !</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Loading Meals...</p>}>
+          <Meals />
+        </Suspense>
+
+      </main>
     </>
   )
 }
